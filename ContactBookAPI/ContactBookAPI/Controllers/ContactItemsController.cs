70156f9a446bea5ffb481ef288616b41ac8e7d41
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-11-23 11:05:14 +1300

    Completed ContactBookAPI
    completed the contact book api with additional image uploading features

--- ContactBookAPI/ContactBookAPI/Controllers/ContactItemsController.cs
+++ ContactBookAPI/ContactBookAPI/Controllers/ContactItemsController.cs
@@ -6,6 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ContactBookAPI.Models;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using ContactBookAPI.Helpers;
+using Microsoft.Extensions.Configuration;
 
 namespace ContactBookAPI.Controllers
 {
@@ -15,9 +20,12 @@ namespace ContactBookAPI.Controllers
     {
         private readonly ContactBookAPIContext _context;
 
-        public ContactItemsController(ContactBookAPIContext context)
+        private IConfiguration _configuration;
+
+        public ContactItemsController(ContactBookAPIContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
         }
 
         // GET: api/ContactItems
@@ -120,6 +128,140 @@ namespace ContactBookAPI.Controllers
         private bool ContactItemExists(int id)
         {
             return _context.ContactItem.Any(e => e.Id == id);
+        }
+
+        // GET: api/Meme/Tags
+        [Route("tags")]
+        [HttpGet]
+        public async Task<List<string>> GetTags()
+        {
+            var contacts = (from m in _context.ContactItem
+                         select m.Tags).Distinct();
+
+            var returned = await contacts.ToListAsync();
+
+            return returned;
+        }
+
+        [HttpPost, Route("upload")]
+        public async Task<IActionResult> UploadFile([FromForm]ContactBookItem meme)
+        {
+            if (!MultipartRequestHelper.IsMultipartContentType(Request.ContentType))
+            {
+                return BadRequest($"Expected a multipart request, but got {Request.ContentType}");
+            }
+            try
+            {
+                using (var stream = meme.Image.OpenReadStream())
+                {
+                    var cloudBlock = await UploadToBlob(meme.Image.FileName, null, stream);
+                    //// Retrieve the filename of the file you have uploaded
+                    //var filename = provider.FileData.FirstOrDefault()?.LocalFileName;
+                    if (string.IsNullOrEmpty(cloudBlock.StorageUri.ToString()))
+                    {
+                        return BadRequest("An error has occured while uploading your file. Please try again.");
+                    }
+
+                    ContactItem memeItem = new ContactItem();
+                    memeItem.Title = meme.Title;
+                    memeItem.Tags = meme.Tags;
+
+                    System.Drawing.Image image = System.Drawing.Image.FromStream(stream);
+                    memeItem.Height = image.Height.ToString();
+                    memeItem.Width = image.Width.ToString();
+                    memeItem.Url = cloudBlock.SnapshotQualifiedUri.AbsoluteUri;
+                    memeItem.Uploaded = DateTime.Now.ToString();
+
+                    _context.ContactItem.Add(memeItem);
+                    await _context.SaveChangesAsync();
+
+                    return Ok($"File: {meme.Title} has successfully uploaded");
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"An error has occured. Details: {ex.Message}");
+            }
+
+
+        }
+
+        private async Task<CloudBlockBlob> UploadToBlob(string filename, byte[] imageBuffer = null, System.IO.Stream stream = null)
+        {
+
+            var accountName = _configuration["AzureBlob:name"];
+            var accountKey = _configuration["AzureBlob:key"]; ;
+            var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            CloudBlobContainer imagesContainer = blobClient.GetContainerReference("images");
+
+            string storageConnectionString = _configuration["AzureBlob:connectionString"];
+
+            // Check whether the connection string can be parsed.
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+                try
+                {
+                    // Generate a new filename for every new blob
+                    var fileName = Guid.NewGuid().ToString();
+                    fileName += GetFileExtention(filename);
+
+                    // Get a reference to the blob address, then upload the file to the blob.
+                    CloudBlockBlob cloudBlockBlob = imagesContainer.GetBlockBlobReference(fileName);
+
+                    if (stream != null)
+                    {
+                        await cloudBlockBlob.UploadFromStreamAsync(stream);
+                    }
+                    else
+                    {
+                        return new CloudBlockBlob(new Uri(""));
+                    }
+
+                    return cloudBlockBlob;
+                }
+                catch (StorageException ex)
+                {
+                    return new CloudBlockBlob(new Uri(""));
+                }
+            }
+            else
+            {
+                return new CloudBlockBlob(new Uri(""));
+            }
+
+        }
+
+        private string GetFileExtention(string fileName)
+        {
+            if (!fileName.Contains("."))
+                return ""; //no extension
+            else
+            {
+                var extentionList = fileName.Split('.');
+                return "." + extentionList.Last(); //assumes last item is the extension 
+            }
+        }
+
+        // GET: api/Meme/Tags
+
+        [HttpGet]
+        [Route("tag")]
+        public async Task<List<ContactItem>> GetTagsItem([FromQuery] string tags)
+        {
+            var memes = from m in _context.ContactItem
+                        select m; //get all the memes
+
+
+            if (!String.IsNullOrEmpty(tags)) //make sure user gave a tag to search
+            {
+                memes = memes.Where(s => s.Tags.ToLower().Equals(tags.ToLower())); // find the entries with the search tag and reassign
+            }
+
+            var returned = await memes.ToListAsync(); //return the memes
+
+            return returned;
         }
     }
 }
