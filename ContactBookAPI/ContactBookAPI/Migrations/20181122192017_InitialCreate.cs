7cf644983de0a846315f15ba9fc17330717a46eb
Cyb3rX3r0 <37169775+Cyb3rX3r0@users.noreply.github.com>
2018-11-23 08:28:41 +1300

    Added Swagger Integration to The Contact Book API
    added swagger integration to the COntact book API

---
+++ ContactBookAPI/ContactBookAPI/Migrations/20181122192017_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactBookAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContactItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(nullable: true),
+                    Url = table.Column<string>(nullable: true),
+                    Tags = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    Uploaded = table.Column<string>(nullable: true),
+                    Width = table.Column<string>(nullable: true),
+                    Height = table.Column<string>(nullable: true),
+                    MobilePhone = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactItem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactItem");
+        }
+    }
+}
--- ContactBookAPI/ContactBookAPI/Startup.cs
+++ ContactBookAPI/ContactBookAPI/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.EntityFrameworkCore;
 using ContactBookAPI.Models;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace ContactBookAPI
 {
@@ -31,6 +32,12 @@ namespace ContactBookAPI
 
             services.AddDbContext<ContactBookAPIContext>(options =>
                      options.UseSqlite(Configuration.GetConnectionString("ContactBookAPIContext")));
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "ContactBook", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +54,18 @@ namespace ContactBookAPI
 
             app.UseHttpsRedirection();
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty; // launch swagger from root
+            });
+
         }
     }
 }
